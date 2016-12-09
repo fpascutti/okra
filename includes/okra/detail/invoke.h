db22c88664a7b26d298bf1bad64c670ba3c4a0ec
Franck Pascutti <franck.pascutti@thomsonreuters.com>
2016-12-09 15:22:02 +0100

    Added `detail::invoke` which is used to invoke a function given regex matches.

---
+++ includes/okra/detail/invoke.h
@@ -0,0 +1,46 @@
+#ifndef OKRA_DETAIL_INVOKE_H
+#define OKRA_DETAIL_INVOKE_H
+
+// Copyright Franck Pascutti 2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstddef>
+#include <tuple>
+#include <utility>
+#include <okra/detail/function_traits.h>
+
+namespace okra { namespace detail {
+
+template<std::size_t Index, std::size_t Processed, std::size_t Arity>
+struct invoker
+{
+    static_assert(Processed < Arity, "Invoking should stop once `Processed == Arity`");
+    static_assert(Processed >= Index, "Number of `Processed` arguments should be at least equal to the number of matches already extracted.");
+    template<typename Matches, typename Fn, typename... Args>
+    void operator()(const Matches& matches, Fn&& fn, Args&&... args) const
+    {
+        return invoker<Index + 1, Processed + 1, Arity>()(matches, std::forward<Fn>(fn), std::forward<Args>(args)..., matches[Index + 1]);
+    }
+};
+template<std::size_t Index, std::size_t Processed>
+struct invoker<Index, Processed, Processed>
+{
+    static_assert(Processed >= Index, "Number of `Processed` arguments should be at least equal to the number of matches already extracted.");
+    template<typename Matches, typename Fn, typename... Args>
+    void operator()(const Matches&, Fn&& fn, Args&&... args) const
+    {
+        return std::forward<Fn>(fn)(std::forward<Args>(args)...);
+    }
+};
+
+template<typename Matches, typename Fn, typename... Args>
+void invoke(const Matches& matches, Fn&& fn, Args&&... args)
+{
+    return invoker<0, sizeof...(Args), std::tuple_size<typename function_traits<Fn>::arguments_type>::value>()(matches, std::forward<Fn>(fn), std::forward<Args>(args)...);
+}
+
+} }
+
+
+#endif // OKRA_DETAIL_INVOKE_H
---
+++ tests/unit-tests/detail/invoke.cpp
@@ -0,0 +1,66 @@
+
+// Copyright Franck Pascutti 2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <string>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <okra/detail/invoke.h>
+#include <okra/re.h>
+
+namespace {
+
+struct invokable
+{
+    void operator()(int n, double& d, const std::string& s0, const std::string& s1) const
+    { return this->call_op(n, d, s0, s1); }
+    MOCK_CONST_METHOD4(call_op, void(int, double&, const std::string&, const std::string&));
+};
+}
+
+TEST(invoke, exact_number_of_matches)
+{
+    double d = 3.14;
+
+    std::string input("Given I wake up in the morning");
+    okra::re::smatch matches;
+    auto match_success = okra::re::regex_match(input, matches, okra::re::regex("Given I (.*) up in the (.*)"));
+    ASSERT_TRUE(match_success) << "Regular expression match should have succeeded.";
+
+    testing::StrictMock<invokable> fn;
+    EXPECT_CALL(fn, call_op(42, testing::Ref(d), testing::StrEq("wake"), testing::StrEq("morning"))).Times(1);
+
+    okra::detail::invoke(matches, fn, 42, d);
+}
+
+TEST(invoke, too_few_matches)
+{
+    double d = 3.14;
+
+    std::string input("Given I wake up in the morning");
+    okra::re::smatch matches;
+    auto match_success = okra::re::regex_match(input, matches, okra::re::regex("Given I .* up in the .*"));
+    ASSERT_TRUE(match_success) << "Regular expression match should have succeeded.";
+
+    testing::StrictMock<invokable> fn;
+    EXPECT_CALL(fn, call_op(42, testing::Ref(d), testing::StrEq(""), testing::StrEq(""))).Times(1);
+
+    okra::detail::invoke(matches, fn, 42, d);
+}
+
+TEST(invoke, too_many_matches)
+{
+    double d = 3.14;
+
+    std::string input("Given I wake up in the morning, happy");
+    okra::re::smatch matches;
+    auto match_success = okra::re::regex_match(input, matches, okra::re::regex("Given I (.*) up in the (.*), (.*)"));
+    ASSERT_TRUE(match_success) << "Regular expression match should have succeeded.";
+
+    testing::StrictMock<invokable> fn;
+    EXPECT_CALL(fn, call_op(42, testing::Ref(d), testing::StrEq("wake"), testing::StrEq("morning"))).Times(1);
+
+    okra::detail::invoke(matches, fn, 42, d);
+}
+
