7209cadcc8bc572074c0e657424f11fab5e378e9
Franck Pascutti <franck.pascutti@thomsonreuters.com>
2017-02-17 11:54:25 +0100

    Added `okra::catch_::framework` and made it so that sample compiles and run fine.

--- samples/inline-scenario/catch.cpp
+++ samples/inline-scenario/catch.cpp
@@ -30,7 +30,7 @@ unsigned long long factorial(unsigned long long)
     return 1ull;
 }
 
-okra::catch::framework framework__;
+okra::catch_::framework framework__;
 auto registry__ = okra::impl(okra::re::basic_regex<okra_char_type>(OKRA_TEXT("Given I have the number (\\d+)")), [](okra::ignore, unsigned long long &input, okra::ignore, const std::basic_string<okra_char_type> &arg) { input = std::stoull(arg); })
 				| okra::impl(okra::re::basic_regex<okra_char_type>(OKRA_TEXT("When I compute its factorial")), [](okra::ignore, unsigned long long input, unsigned long long &result) { result = factorial(input); })
 				| okra::impl(okra::re::basic_regex<okra_char_type>(OKRA_TEXT("Then I see the number (\\d+)")), [](okra::ignore, unsigned long long input, unsigned long long result, const std::basic_string<okra_char_type> &arg) { auto exp = std::stoull(arg); REQUIRE(exp == result); });
@@ -40,51 +40,56 @@ auto registry__ = okra::impl(okra::re::basic_regex<okra_char_type>(OKRA_TEXT("Gi
 TEST_CASE("Factorial")
 {
 
-    SECTION("Factorial of 0")
-    {
-        okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 0"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Given I have the number 0"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Then I see the number 1"), __FILE__, __LINE__);
+	SECTION("Factorial of 0")
+	{
+		okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 0"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Given I have the number 0"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Then I see the number 1"), __FILE__, __LINE__);
 
-        unsigned long long input, result;
-        s(framework__, registry__, input, result);
-    }
+		unsigned long long input, result;
+		s(framework__, registry__, input, result);
+	}
 
-    SECTION("Factorial of 1")
-    {
-        okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 1"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Given I have the number 1"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Then I see the number 1"), __FILE__, __LINE__);
+	SECTION("Factorial of 1")
+	{
+		okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 1"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Given I have the number 1"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Then I see the number 1"), __FILE__, __LINE__);
 
-        unsigned long long input, result;
-        s(framework__, registry__, input, result);
-    }
+		unsigned long long input, result;
+		s(framework__, registry__, input, result);
+	}
 
-    SECTION("Factorial of 2")
-    {
-        okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 2"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Given I have the number 2"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Then I see the number 2"), __FILE__, __LINE__);
+	SECTION("Factorial of 2")
+	{
+		okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 2"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Given I have the number 2"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Then I see the number 2"), __FILE__, __LINE__);
 
-        unsigned long long input, result;
-        s(framework__, registry__, input, result);
-    }
+		unsigned long long input, result;
+		s(framework__, registry__, input, result);
+	}
 
-    SECTION("Factorial of 3")
-    {
-        okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 3"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Given I have the number 3"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
-        s.add_step(OKRA_TEXT("Then I see the number 6"), __FILE__, __LINE__);
+	SECTION("Factorial of 3")
+	{
+		okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Factorial of 3"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Given I have the number 3"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("When I compute its factorial"), __FILE__, __LINE__);
+		s.add_step(OKRA_TEXT("Then I see the number 6"), __FILE__, __LINE__);
 
-        unsigned long long input, result;
-        s(framework__, registry__, input, result);
-    }
+		unsigned long long input, result;
+		s(framework__, registry__, input, result);
+	}
 
-    SECTION("Factorial of 4")
+}
+
+TEST_CASE("Square")
+{
+
+    SECTION("Square of 4")
     {
         okra::basic_scenario<okra_char_type> s(OKRA_TEXT("Square of 4"), __FILE__, __LINE__);
         s.add_step(OKRA_TEXT("Given I have the number 4"), __FILE__, __LINE__);
